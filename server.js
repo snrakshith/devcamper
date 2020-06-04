85a640942191959c4044584e0a5bff7a52587494
Rakshith <snrakshith.97@gmail.com>
2020-06-04 12:39:05 +0530

    added _data and models folder

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
 
 // Import Route Files
 const bootcampsRoute = require("./routes/bootcamps");
@@ -12,6 +14,11 @@ dotenv.config({
 // Inistalize an Express Router
 const app = express();
 
+// Dev Logging Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 // Mount Routes
 app.use("/api/v1/bootcamps", bootcampsRoute);
 
@@ -20,7 +27,9 @@ const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 app.listen(
   PORT,
-  console.log(`Server is running on ${NODE_ENV} mode at ${PORT}`)
+  console.log(
+    `Server is running on ${NODE_ENV} mode at ${PORT}..`.yellow.underline.bold
+  )
 );
 
 // console.log("Hi");
