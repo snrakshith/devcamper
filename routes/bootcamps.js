238dc7e13321c4701dd11c356c615ee38c78d066
Rakshith <snrakshith.97@gmail.com>
2020-06-04 08:44:22 +0530

    new file:   config/db.js

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,6 @@ router.route("/").get(getBootcamps).post(createBootcamp);
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(createBootcamp)
+  .put(updateBootcamp)
   .delete(deleteBootcamps);
 module.exports = router;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use("/api/v1/bootcamps", bootcampsRoute);
 
 // Define Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 app.listen(
   PORT,
